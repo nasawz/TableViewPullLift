62e6eea49b4d6253a862b103de110c9def757eab
nasa <nasa127@gmail.com>
2011-07-06 10:43:54 +0800

    Initial Commit

---
+++ TableViewPullLift/TableViewPullLiftAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TableViewPullLiftAppDelegate.h
+//  TableViewPullLift
+//
+//  Created by zhe wang on 11-7-6.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewPullLiftAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ TableViewPullLift/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TableViewPullLift
+//
+//  Created by zhe wang on 11-7-6.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
